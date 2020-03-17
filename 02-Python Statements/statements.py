69ba2a3b7cbcd941a567c7e4744cfa8721480965
f121t2 <f121t12917@gmail.com>
2020-03-17 22:02:33 +0800

    Methods and Functions

--- 02-Python Statements/statements.py
+++ 02-Python Statements/statements.py
@@ -70,3 +70,42 @@ print(randint(0, 100))
 result = input('Enter a number here: ')
 print(result)
 print(type(result)) # 輸入的數值為字串
+
+mystring = 'hello'
+mylist = []
+for letter in mystring:
+    mylist.append(letter)
+
+# 可改為以下
+mylist = [letter for letter in mystring]
+print(mylist)
+
+mylist = [x for x in 'word']
+print(mylist)
+
+
+mylist = [num**2 for num in range(0, 11)]
+print(mylist)
+
+# 加入判斷
+mylist = [x for x in range(0, 11) if x%2 == 0]
+print(mylist)
+
+mylist = [x if x%2 == 0 else 'ODD' for x in range(0, 11)]
+
+celcius = [0, 10, 20, 34.5]
+fahrenheit = [( (9/5) * temp + 32) for temp in celcius]
+print(fahrenheit)
+# 上面式子等於下面
+fahrenheit = []
+for temp in celcius:
+    fahrenheit.append(( (9/5) * temp + 32))
+print(fahrenheit)
+
+
+for x in [2, 4, 6]:
+    for y in [1, 100, 1000]:
+        mylist.append(x * y)
+
+mylist = [x*y for x in [2, 4, 6] for y in [1, 10, 1000]]
+print(mylist)
